8709069568b5a0cde03813e8675849a73bcff47e
kiyeonlee7 <kiyeonlee7@gmail.com>
2019-01-03 18:10:01 -0600

    Add files via upload

---
+++ connect/README.md
@@ -0,0 +1,12 @@
+User Manual
+How to compile the project:
+1)	Copy all the files into your CS50 IDE
+2)	In terminal window, type “cd/implementation” and then “Flask Run”
+3)	In the terminal window, click the link that shows (e.g. http://ide50-kiyeonlee.cs50.io:8080/)
+4)	Enter a dining hall (such as Annenberg) and find a table to sit at. On the website, check-in by entering your name, time (that you want your entry to expire), location (in the dining hall or your identifying features), and (conversation) interests. For example, name = Joe Smith; time leaving = 08:00 PM; location= Table A5; interests= CS50, music, finance. Other people entering the dining hall can also type in their own information.
+5)	After pressing submit, you should see your entry below the form. On their devices (including laptop, tablet, and mobile), other users will be able to scroll through the list of available students and see your information. If people are interested in joining you at your table, they will also check-in by entering their information and the same location that you’ve entered.
+6)	If you’ve mistyped any information, you can edit your entry by pressing the edit button on the right of your row. Press submit when done editing.
+7)	If you’d like to delete your entry (perhaps you’re leaving earlier), then press the delete button next to the edit button. Your entry will disappear from the list.
+8)	Engage in deep life conversations and make new friends.
+
+Video explanation: https://youtu.be/s-Oh63Xbvz8
---
+++ connect/application.py
@@ -0,0 +1,118 @@
+import os
+import datetime
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
+from pytz import timezone
+from helpers import apology, login_required, lookup, usd
+
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///connect.db")
+
+# Convert a string value to a time value
+def convert(timeString):
+    time = timeString.split(":")
+    minute = int(time[0])*60 + int(time[1])
+    return minute
+
+# The default time shown is current time plus one hour
+def showtime():
+    now = datetime.datetime.now(timezone('US/Eastern'))
+    ahead = now + datetime.timedelta(hours=1)
+    return ahead.strftime("%H:%M")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    # POST
+    if request.method == "POST":
+        # Validate form submission
+        if not request.form.get("name"):
+            return apology("Name is required")
+        elif not request.form.get("time"):
+            return apology("Expiration time is required")
+        # Validate time (must be after current time)
+        now = datetime.datetime.now(timezone('US/Eastern'))
+        # Format time
+        current = f"{now.hour}:{now.minute}"
+        # Validate time
+        if (convert(request.form.get("time")) < convert(current)):
+            return apology("Must enter time after current")
+        else:
+            flash("Checked-In!")
+
+        # Enter submission into database
+        db.execute("INSERT INTO 'Users' ('name','time','location','interests') VALUES(:name, :time, :location, :interests)",
+                    name=request.form.get("name"), time=request.form.get("time"), location=request.form.get("location"), interests=request.form.get("interests"))
+        # Sort rows by oldest time first
+        rows = db.execute("SELECT * FROM Users ORDER BY time ASC")
+        # Return to homepage after sorting
+        return redirect("/")
+
+
+    # GET
+    else:
+        # Get rows from database and sort users by time leaving
+        rows = db.execute("SELECT * FROM Users ORDER BY time ASC")
+
+        # Compare times and delete rows with expired times
+        now = datetime.datetime.now(timezone('US/Eastern'))
+        current = f"{now.hour}:{now.minute}"
+        rows = db.execute("SELECT * FROM Users")
+
+        for row in rows:
+            # Delete entries that have invalid times
+            if convert(row["time"]) < convert(current):
+                db.execute("DELETE FROM Users WHERE id=:id", id=row['id'])
+        # Get rows from database and sort users by time leaving
+        rows = db.execute("SELECT * FROM Users ORDER BY time ASC")
+        # Render user list
+        return render_template("index.html", rows=rows, prefill=None, showtime=showtime())
+
+# Delete entry function
+@app.route("/delete/<int:id>")
+def delete(id):
+    db.execute("DELETE FROM Users WHERE id=:id", id=id)
+    # Get rows from database and sort users by time leaving
+    rows = db.execute("SELECT * FROM Users ORDER BY time ASC")
+    return redirect("/")
+
+# Edit entry function
+@app.route("/edit/<int:line>")
+def edit(line):
+    # Query row from database
+    row = db.execute("SELECT * FROM Users WHERE id=:line", line=line)
+    # Remove row from database
+    db.execute("DELETE FROM Users WHERE id=:line", line=line)
+	# Get rows from database and sort users by time leaving
+    rows = db.execute("SELECT * FROM Users ORDER BY time ASC")
+
+    # Render index.html with form fields pre-populated from the deleted row  -- how prepopulate?
+    return render_template("index.html", prefill=row[0], rows=rows)
